1039b86f41f2c72c7bb26eff3340cdb2e0ce6d47
AndrewAndrea <z_huan0619@163.com>
2021-04-09 17:33:42 +0800

    更新简书上传图片操作，自动去掉后缀上传

--- app_doc/spider/jianshu_publish.py
+++ app_doc/spider/jianshu_publish.py
@@ -11,6 +11,7 @@ import requests
 import arrow
 
 from lxml import etree
+from requests_toolbelt import MultipartEncoder
 from app_doc.spider.utils.tools import headers_to_dict
 
 
@@ -24,7 +25,6 @@ class JianShuPublish:
             Accept: application/json
             Accept-Encoding: gzip, deflate, br
             Accept-Language: zh-CN,zh;q=0.9
-            Content-Type: application/json; charset=UTF-8
             Cookie: {cookie}
             Host: www.jianshu.com
             Origin: https://www.jianshu.com
@@ -60,25 +60,43 @@ class JianShuPublish:
         return False
 
     def upload_img(self, content):
-        upload_url = 'https://www.jianshu.com/upload_images/fetch'
+        upload_head = """
+            accept: application/json
+            accept-language: zh-CN,zh;q=0.9
+            origin: https://www.jianshu.com
+            referer: https://www.jianshu.com/
+            user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36
+        """
+        upload_url = 'https://upload.qiniup.com/'
         md = markdown.Markdown(extensions=['markdown.extensions.extra', 'markdown.extensions.codehilite'])
         content_html = md.convert(content)
         res_html = etree.HTML(content_html)
         src_list = res_html.xpath('//img/@src')
         for src in src_list:
-            while 1:
-                src = src.rsplit('.', 1)[0] + '.png'
-                form_data = {
-                    "url": src
-                }
-                res = self.sess.post(url=upload_url, json=form_data)
-                if res.text:
-                    r_json = res.json()
-                    # 图片在文章中本来就不存在
-                    img_url = r_json.get('url')
-                    content = content.replace(src, img_url)
-                    time.sleep(random.random() * 0.1)
-                    break
+            res_img = requests.get(src)
+            file_name = src.rsplit('/', 1)[1].rsplit('.')[0] + '.png'
+            token_url = f'https://www.jianshu.com/upload_images/token.json?filename={file_name}'
+            res_token = self.sess.get(token_url)
+            if res_token.text:
+                res_token_json = res_token.json()
+                token = res_token_json.get('token')
+                key = res_token_json.get('key')
+                if token and key:
+                    m = MultipartEncoder(
+                        fields={"file": (file_name, res_img.content),
+                                'token': token, 'key': key, 'x:protocol': 'https'})
+                    headers_dict = headers_to_dict(upload_head)
+                    headers_dict['Content-Type'] = m.content_type
+
+                    res_upload_file = requests.post(url=upload_url, data=m, headers=headers_dict)
+                    if res_upload_file.text:
+                        r_json = res_upload_file.json()
+                        if r_json.get('error'):
+                            continue
+                        # 图片在文章中本来就不存在
+                        img_url = r_json.get('url')
+                        content = content.replace(src, img_url)
+                        time.sleep(random.random())
         return content
 
     # 文章保存
